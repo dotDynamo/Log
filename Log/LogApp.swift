9dfede206002b0dfd27eb90929201ba0d5b0d4fb
doyDynamo <dho2000@hotmail.com>
2026-01-06 21:09:40 -0600

    Initial Commit

---
+++ Log/LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogApp.swift
+//  Log
+//
+//  Created by Diego Herrera on 2026/01/06.
+//
+
+import SwiftUI
+
+@main
+struct LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
