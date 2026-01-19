a87b5dda43e9c738418ef6d25692905e09b5afcd
doyDynamo <dho2000@hotmail.com>
2026-01-18 19:35:27 -0600

    refactor: add placeholders for LogService's methods

--- Log/Services/LogService.swift
+++ Log/Services/LogService.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+struct LogService{
+    let modelContext: ModelContext
+    
+    func addMovieLog(){
+        
+    }
+    
+    func addSeriesLog(){
+        
+    }
+    
+    func addMusicLog(title: String, artist: String, releaseType: ReleaseType, satus: Status, releaseDate: Date? = nil, startDate: Date? = nil, finishDate: Date? = nil, ){
+        
+    }
+    
+    func addBookLog(){
+        
+    }
+    
+    func addGameLog(){
+        
+    }
+    
+}
+
+/*
+, rating: Double? = nil, recommendedBy: String? = nil, notes: String? = nil, status: Status = Status.inQueue, user: User, artist: String, album: String? = nil, releaseType: ReleaseType = .single
+ */
