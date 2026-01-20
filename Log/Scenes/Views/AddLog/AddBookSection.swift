0d3f569ca0658fb6f71f586dc0f893ac2da0d14f
doyDynamo <dho2000@hotmail.com>
2026-01-19 21:38:43 -0600

    feat: add book specific section

--- Log/Scenes/Views/AddLog/AddBookSection.swift
+++ Log/Scenes/Views/AddLog/AddBookSection.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct AddBookSection: View {
+    @Binding var author: String
+    @Binding var isbn: String
+    
     var body: some View {
-        
+        TextField("Author", text: $author)
+        TextField("ISBN", text: $isbn)
     }
 }
