11d540922af0c635fe98ddbf88102f4e053ff4f8
doyDynamo <dho2000@hotmail.com>
2026-02-03 21:56:53 -0600

    fix: detail view did not scroll

--- Log/Scenes/Views/Details/LogDetailView.swift
+++ Log/Scenes/Views/Details/LogDetailView.swift
@@ -10,42 +10,44 @@ import SwiftUI
 struct LogDetailView: View {
     let log: Log
     var body: some View {
-        VStack(alignment: .leading){
-            HStack{
-                VStack(alignment: .leading){
-                    Text(TimeUtils.getFromDate(log.releaseDate, get: .dateTime.year()))
-                    Text(log.title).font(.title).bold()
+        ScrollView{
+            VStack(alignment: .leading){
+                HStack{
+                    VStack(alignment: .leading){
+                        Text(TimeUtils.getFromDate(log.releaseDate, get: .dateTime.year()))
+                        Text(log.title).font(.title).bold()
+                    }
+                    Spacer()
+                    RatingView(rating: log.rating, size: .large, style: .round)
                 }
+                Divider()
+                switch log.category{
+                case .movie:
+                    MovieDetailView(log: log)
+                case .series:
+                    SeriesDetailView(log: log)
+                case .music:
+                    MusicDetailView(log: log)
+                case .book:
+                    BookDetailView(log: log)
+                case .game:
+                    GameDetailView(log: log)
+                }
+                DatePeriodView(startDate: log.startDate, finishDate: log.finishDate)
+                if log.recommendedBy != nil {
+                    Divider()
+                    Text("Recommended by:").font(.headline).foregroundStyle(.gray)
+                    Text(log.recommendedBy!)
+                }
+                if log.notes != nil {
+                    Divider()
+                    Text("Notes:").font(.headline).foregroundStyle(.gray)
+                    Text(log.notes!)
+                }
+                Divider()
+                TagView(tags: log.tags)
                 Spacer()
-                RatingView(rating: log.rating, size: .large, style: .round)
             }
-            Divider()
-            switch log.category{
-            case .movie:
-                MovieDetailView(log: log)
-            case .series:
-                SeriesDetailView(log: log)
-            case .music:
-                MusicDetailView(log: log)
-            case .book:
-                BookDetailView(log: log)
-            case .game:
-                GameDetailView(log: log)
-            }
-            DatePeriodView(startDate: log.startDate, finishDate: log.finishDate)
-            if log.recommendedBy != nil {
-                Divider()
-                Text("Recommended by:").font(.headline).foregroundStyle(.gray)
-                Text(log.recommendedBy!)
-            }
-            if log.notes != nil {
-                Divider()
-                Text("Notes:").font(.headline).foregroundStyle(.gray)
-                Text(log.notes!)
-            }
-            Divider()
-            TagView(tags: log.tags)
-            Spacer()
         }
         .padding()
         .navigationTitle("Log")
